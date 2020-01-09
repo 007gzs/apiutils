d889cbfd230d46c5fd1d438c61ea6a26fa33b47e
007 <007gzs@gmail.com>
2020-01-09 13:58:34 +0800

    tests

---
+++ tests/tests.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class UtilityTestCase(unittest.TestCase):
+
+    def test_object_dict(self):
+        obj = ObjectDict()
+        self.assertTrue(obj.xxx is None)
+        obj.xxx = 1
+        self.assertEqual(1, obj.xxx)
